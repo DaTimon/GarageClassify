886b622c5d9f4ed7309fe8e849787404906e7cc7
DaTimon <78351648+DaTimon@users.noreply.github.com>
2022-09-02 13:28:55 +0800

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -23,10 +23,10 @@ function beforeUpload(file) {
     message.error("You can only upload JPG/PNG file!");
   }
 
-  const isLt2M = file.size / 1024 / 1024 < 2;
+  const isLt2M = file.size / 1024 / 1024 < 20;
 
   if (!isLt2M) {
-    message.error("Image must smaller than 2MB!");
+    message.error("Image must smaller than 20MB!");
   }
 
   return isJpgOrPng && isLt2M;
